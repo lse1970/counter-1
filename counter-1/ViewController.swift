418f54c16b664d33615cf8288349ec749eafa7d3
D7702_09 <d7702_09@d7702-09ui-imac.local>
2018-03-20 15:42:47 +0900

    reset

--- counter-1/ViewController.swift
+++ counter-1/ViewController.swift
@@ -22,6 +22,10 @@ class ViewController: UIViewController {
         
     }
     
+    @IBAction func btrG(_ sender: Any) {
+        count = 0
+        lbl.text = "\(count)"
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
